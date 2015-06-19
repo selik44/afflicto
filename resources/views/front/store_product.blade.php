65dd2fbf64f499b078adf27095b919dd3d6458c6
Petter Thowsen <me@afflicto.net>
2015-06-19 10:54:34 +0200

    improved product display

--- resources/views/front/store_product.blade.php
+++ resources/views/front/store_product.blade.php
@@ -32,8 +32,7 @@
 				</div>
 			</div>
 
-			<div class="product-actions col-l-4 col-m-5 col-sm-6 tight-right">
-
+			<div class="product-info col-l-4 col-m-5 col-sm-6 tight-right">
 				<form class="vertical" action="{{url('cart')}}" method="POST">
 					<input type="hidden" name="_token" value="{{csrf_token()}}">
 					<input type="hidden" name="product_id" value="{{$product->id}}">
@@ -50,8 +49,12 @@
 	                    @endforeach
 	                </div>
 
-					<input class="huge primary huge buy" style="width: 100%;" type="submit" name="BUY" value="@lang('store.buy')" class="success">
+					<button class="huge primary huge buy" style="width: 100%;" type="submit" name="BUY"><i class="fa fa-cart-plus"></i> @lang('store.buy')</button>
 				</form>
+
+				<p class="lead summary">
+					{{$product->summary}}
+				</p>
 			</div>
 		</div>
 
@@ -60,19 +63,35 @@
 		<div class="product-bottom col-xs-12 tight">
 			<ul id="product-tabs" class="nav tabs clearfix">
 				<li class="current"><a href="#product-info">Product Info</a></li>
-				<li><a href="#product-related">Related Products</a></li>
-				<li><a href="#product-reviews">Reviews</a></li>
+				<li><a href="#product-relations">Related Products</a></li>
 			</ul>
 
 			<div class="tab" id="product-info">
 				{!! $product->description !!}
 			</div>
 
-			<div class="tab" id="product-related">
-				<ul>
-					<li>Something</li>
-					<li>Else</li>
-					<li>Here</li>
+			<div class="tab" id="product-relations">
+				<ul class="relations flat">
+					@foreach($product->relations as $related)
+						<li class="related clearfix">
+							<div class="row">
+								<div class="thumbnail pull-left">
+									<a href="{{$related->getPath()}}">
+										<img class="image" src="{{asset('images/products/' .$related->images[0]->name)}}" alt=""/>
+									</a>
+								</div>
+								<div class="info pull-left">
+									<a class="link" href="{{$related->getPath()}}">
+										<h4><strong>{{$related->manufacturer->name}}</strong> {{$product->name}}</h4>
+									</a>
+
+									<div class="price">
+										<h4><strong>{{$related->price}},-</strong></h4>
+									</div>
+								</div>
+							</div>
+						</li>
+					@endforeach
 				</ul>
 			</div>
 		</div>
