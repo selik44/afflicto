4dd79e6df3f5b863058db527bebdea868c460d30
Petter Thowsen <me@afflicto.net>
2015-07-14 00:23:18 +0200

    fixed front navigation to show all root categories

--- resources/views/front/partial/navigation_friluft.blade.php
+++ resources/views/front/partial/navigation_friluft.blade.php
@@ -3,7 +3,7 @@ use Friluft\Category;
 
 echo '<li><a href="' .route('home') .'">Home</a></li>';
 
-foreach (Category::root()->orderBy('order', 'desc')->get() as $cat) {
+foreach (Category::root()->orderBy('order', 'asc')->get() as $cat) {
     echo '<li>' .$cat->renderMenuItem('store', [], true);
 
         echo '<div class="nav-dropdown"><div class="inner clearfix">';
