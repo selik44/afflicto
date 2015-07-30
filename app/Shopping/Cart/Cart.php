800b0993b417534a768dc4179be5a198d4980bd0
Petter Thowsen <me@afflicto.net>
2015-07-30 10:13:03 +0200

    y

--- app/Shopping/Cart/Cart.php
+++ app/Shopping/Cart/Cart.php
@@ -296,9 +296,9 @@ class Cart {
 		$data['merchant'] = [
 			'id' => getenv('KLARNA_MERCHANT_ID'),
 			'terms_uri' => url('terms-and-conditions'),
-			'checkout_uri' => url('store/checkout'),
-			'confirmation_uri' => url('store/success') .'?klarna_order={checkout.order.uri}',
-			'push_uri' => url('store/push') .'?klarna_order={checkout.order.uri}',
+			'checkout_uri' => url('checkout'),
+			'confirmation_uri' => url('success') .'?klarna_order={checkout.order.uri}',
+			'push_uri' => url('push') .'?klarna_order={checkout.order.uri}',
 		];
 
 		$order->create($data);
