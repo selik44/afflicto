d0eade56c49c69a324d860e05d41b86f77fb9c3a
Petter Thowsen <me@afflicto.net>
2015-08-10 20:48:40 +0200

    y

--- resources/views/front/user_order.blade.php
+++ resources/views/front/user_order.blade.php
@@ -11,7 +11,7 @@
 @section('article')
     <div class="paper row" style="padding: 1rem;">
         <h2>@lang('store.order') {{$order->created_at->format('d M Y')}}</h2>
-        <h6>Ordrenummer: #{{$order->id}}</h6>
+        <h4>Ordrenummer: #{{$order->id}}</h4>
 
         @if($order->status == 'delivered')
             <p class="lead color-success">@lang('store.order status.delievered')</p>
