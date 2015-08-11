3b510798fb7e7d7a0e98304691dd7d610b48e352
Petter Thowsen <me@afflicto.net>
2015-08-11 06:21:39 +0200

    y

--- app/Console/Commands/GenerateThumbnails.php
+++ app/Console/Commands/GenerateThumbnails.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Console\Command;
 use Friluft\Product;
+use Symfony\Component\Finder\Iterator\RecursiveDirectoryIterator;
 
 class GenerateThumbnails extends Command {
 
@@ -24,12 +25,44 @@ class GenerateThumbnails extends Command {
 		parent::__construct();
 	}
 
+	public function fire() {
+		$iterator = new \DirectoryIterator(public_path('images/products'));
+		foreach($iterator as $file) {
+			if ($file->isDot() || $file->isDir()) continue;
+
+			# is this already a thumbnail? if so, delete it.
+			if (preg_match('/_thumbnail\.[a-zA-Z0-9]+$/', $file->getFilename()) === 1) {
+				unlink($file->getRealPath());
+				continue;
+			}
+			$this->comment('reading ' .$file->getFilename());
+			try {
+				$img = \Img::make($file);
+				$img->resize(800, null, function ($constraint) {
+					$constraint->upsize();
+					$constraint->aspectRatio();
+				})->save();
+
+				$info = $file->getFileInfo();
+				$thumbnail = $info->getPath() .'/' .$info->getBasename('.' .$info->getExtension()) .'_thumbnail.' .$info->getExtension();
+
+				# generate thumbnail
+				$img->resize(null, 200, function ($constraint) {
+					$constraint->upsize();
+					$constraint->aspectRatio();
+				})->save($thumbnail);
+			}catch(\Exception $e) {
+				$this->comment('Error: ' .$e->getMessage());
+			}
+		}
+	}
+
 	/**
 	 * Execute the console command.
 	 *
 	 * @return mixed
 	 */
-	public function fire()
+	public function fire_old()
 	{
 		foreach(Product::all() as $product) {
 			foreach($product->images as $image) {
