8c7bf93721a3413f0ade53270482489f719f6f03
Petter Thowsen <me@afflicto.net>
2015-08-27 03:47:22 +0200

    added dashboard stats

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,9 +1,13 @@
 <?php namespace Friluft\Http\Controllers\Admin;
 
+use Carbon\Carbon;
 use Friluft\Http\Requests;
 use Friluft\Http\Controllers\Controller;
 
+use Friluft\Order;
+use Friluft\Product;
 use Illuminate\Http\Request;
+use Input;
 
 class DashboardController extends Controller {
 
@@ -14,7 +18,50 @@ class DashboardController extends Controller {
 	 */
 	public function index()
 	{
-		return $this->view('admin.dashboard');
+		if (Input::has('from')) {
+			list($year, $month, $day) = explode('-', Input::get('from'));
+			$from = Carbon::createFromDate($year, $month, $day);
+		}else {
+			$from = Carbon::now()->subMonth(1)->setTime(0,0,0);
+		}
+
+		if (Input::has('to')) {
+			list($year, $month, $day) = explode('-', Input::get('to'));
+			$to = Carbon::createFromDate($year, $month, $day);
+		}else {
+			$to = Carbon::now()->subMonth(1)->setTime(0,0,0);
+		}
+
+		$labels = [];
+		$values = [];
+
+		# loop through the days of the interval
+		$current = $from->copy();
+		for($i = $from->copy(); $i->timestamp < $to->timestamp; $i->addDay()) {
+			$min = $i->copy()->setTime(0, 0, 0);
+			$max = $i->copy()->setTime(23, 59, 59);
+
+			# add a label for this date
+			$labels[] = $i->toDateString();
+
+			# calculate the total profit for this day
+			$profit = 0;
+			foreach(Order::where('created_at', '>=', $min->timestamp)->where('created_at', '<=', $max->timestamp)->get() as $order) {
+				# get profit
+				foreach($order->items as $item) {
+					$product = Product::find($item['reference']['id']);
+					$profit += $item['total_price_excluding_tax'] - $product->inprice * $item['quantity'];
+				}
+			}
+
+			$values[] = $profit;
+		}
+
+		if (Input::has('from')) dd(['values' => $values, 'labels' => $labels]);
+
+		return $this->view('admin.dashboard')->with([
+
+		]);
 	}
 
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,58 +5,70 @@
 @stop
 
 @section('header')
-    <h2 class="title">@lang('admin.dashboard')</h2>
+	<h2 class="title">@lang('admin.dashboard')</h2>
 @stop
 
-@section('page')
-	<div class="col-m-6 col-l-4">
-		<div class="module">
-			<div class="module-header"><h6>New Orders</h6></div>
-			<div class="module-content">
-				<ul class="flat">
-					<li><a href="#">Someone bought something (#23)</span></a></li>
-					<li><a href="#">Some order (#24)</span></a></li>
-					<li><a href="#">Another one (#25)</span></a></li>
-				</ul>
+@section('scripts')
+	@parent
+
+	<script src="http://cdn.zingchart.com/zingchart.min.js"></script>
+
+	<script>
+
+		var data = {
+			type: "line",
+			'scale-x': {
+				'label': {
+					'text': 'Date',
+					'values': [1,2,3,4,5],
+				},
+			},
+			'scale-y': {
+				'label': {
+					'text': 'Profit',
+				},
+				"format": "%v",
+				"negation": "currency",
+				"thousands-separator":",",
+			},
+			'plot': {
+				"format": "%v,-",
+				"negation": "currency",
+				"thousands-separator":",",
+			},
+			series: [
+				{
+					values: [249, 2601, 635, 0, 15234/*, 520, 724, 6346, 9592, 246, 592, 4343, 49, 249, 2601, 635, 0, 15234, 520, 724, 6346, 9592, 246, 592, 4343, 49, 531, 0, 0, 0, 631*/]
+				}
+			]
+		};
+
+		zingchart.render({
+			id: 'profit-chart',
+			data: data,
+		});
+	</script>
+@stop
+
+@section('content')
+	<div class="col-xs-6">
+		<div class="module" id="profit">
+			<div class="module-header clearfix">
+				<h6 class="pull-left">@lang('admin.profit')</h6>
+				<form action="{{route('admin.dashboard')}}" method="GET" class="inline pull-right">
+					<label for="from">Fra
+						<input type="date" name="from">
+					</label>
+
+					<label for="to">Til
+						<input type="date" name="to">
+					</label>
+
+					<input type="submit" class="success" value="Oppdater">
+				</form>
 			</div>
-		</div>
-	</div>
-
-	<div class="col-m-6 col-l-4">
-		<div class="module">
-			<div class="module-header"><h6>Something Else</h6></div>
 			<div class="module-content">
-				<ul class="flat">
-					<li><a href="#">Some order</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-
-	<hr class="visible-m">
-
-	<div class="col-m-6 col-l-4">
-		<div class="module">
-			<div class="module-header"><h6>New Orders</h6></div>
-			<div class="module-content">
-				<ul class="flat">
-					<li><a href="#">Someone bought something (#23)</span></a></li>
-					<li><a href="#">Some order (#24)</span></a></li>
-					<li><a href="#">Another one (#25)</span></a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	
-	<hr class="visible-l-up">
-
-	<div class="col-m-6 col-l-4">
-		<div class="module">
-			<div class="module-header"><h6>Something Else</h6></div>
-			<div class="module-content">
-				<ul class="flat">
-					<li><a href="#">Some order</a></li>
-				</ul>
+				<div id="profit-chart"></div>
 			</div>
 		</div>
 	</div>
