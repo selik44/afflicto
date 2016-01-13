031fedbf1087764d3e47843b8585524d0c58893e
Petter Thowsen <me@afflicto.net>
2016-01-13 22:42:39 +0100

    bug fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,5 @@
 <?php
 
-get('/test', function() {
-	$kombo = \Friluft\Product::whereName('Test Kombo')->first();
-	$kombo->getAvailability();
-});
-
 # HOME ROUTES
 get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 get('search', ['as' => 'search', 'uses' => 'SearchController@index']);
