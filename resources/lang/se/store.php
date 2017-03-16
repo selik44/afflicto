1b323ad19017ebf03308fec36a27aafdb3bddb6c
root <root@247sport.tk>
2017-03-16 03:28:12 -0400

    a few bugfixes from production server

--- resources/lang/se/store.php
+++ resources/lang/se/store.php
@@ -1,9 +1,90 @@
 <?php
 
 return [
-	
-	'welcome' => 'Välkommen till',
-	'buy now' => 'köp nu',
-	'add to cart' => 'Lägg till i kundvagn',
+	'all' => 'Alle',
+	'search' => 'Søk',
+	'help' => 'Hjelp',
+	'account' => 'Konto',
+	'my account' => 'Min Konto',
+	'my orders' => 'Mine Bestillinger',
+	'order' => 'Bestilling',
+	'log in' => 'Logg inn',
+	'log out' => 'Logg ut',
+	'register' => 'Registrer',
+	'status' => 'Status',
+	'cart' => 'Handlekurv',
+	'checkout' => 'Kassen',
+	'to checkout' => 'Til Kassen',
+	'to cart' => 'Til Handlekurven',
+	'place order' => 'Fullfør Bestilling',
+	'add to cart' => 'Legg i Kurven',
+	'popular products' => 'Populære Produkter',
+	'products' => 'Produkter',
+	'product info' => 'Teknisk Informasjon',
+	'related products' => 'Relaterte Produkter',
+	'home' => 'Hjem',
+	'login' => 'Logg in',
+	'total' => 'Totalt',
+	'quantity' => 'Antall',
+	'price' => 'Pris',
+	'product' => 'Produkt',
+	'cart empty' => 'Handlekurven er tom.',
+	'done' => 'Ferdig',
+	'manufacturer' => 'Produsent',
+	'about' => 'Om',
+	'here' => 'her',
 
+	'in stock' => 'På Lager',
+	'out of stock' => 'Utsolgt',
+
+	'product added to cart' => 'Produktet er lagt i kurven',
+
+	'your cart is empty' => 'Handlekurven er tom!',
+
+	'auth' => [
+		'register' => 'Ny Konto',
+		'forgot your password?' => 'Glemt passordet ditt?',
+		'create account' => 'Opprett Konto',
+		'forgot password' => 'Glemt Passord',
+		'reset password' => 'Tilbakestill Passord',
+		'new password' => 'Nytt Passord',
+	],
+
+	'shipping' => [
+		'shipping' => 'Frakt',
+		'service-pack' => 'Service-pakke med Sporing',
+		'mail' => 'Brevpost',
+	],
+
+	'sort' => [
+		'sort' => 'Sorter',
+		'price ascending' => 'Pris, Lav - Høy',
+		'price descending' => 'Pris, Høy - Lav',
+		'in stock' => 'På Lager',
+	],
+
+	'search no results' => 'Fant ingenting.',
+	'search results' => 'Du søkte på ',
+
+	'order status' => [
+		'delivered' => 'Levert',
+		'not delievered' => 'Ikke Levert',
+	],
+
+	'user' => [
+		'my account' => 'Min Konto',
+		'my orders' => 'Mine Bestillinger',
+		'settings' => 'Instillinger',
+		'settings saved' => 'Instillinger lagret.',
+	],
+
+	'crumbs' => [
+		'home' => 'Hjem',
+		'user' => 'Konto',
+		'user_settings' => 'Instillinger',
+		'user_order' => 'Bestilling',
+		'search' => 'Søk',
+		'checkout' => 'Kassen',
+		'success' => 'Ferdig',
+	],
 ];
